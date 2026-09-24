8502f1023f31e1915882dd9cdc5e6aabd89c1138
Matt821115 <821115@mystma.org>
2026-09-24 14:12:49 -0500

    madlib1

---
+++ Unit 2/simple_mad_lib.py
@@ -0,0 +1,10 @@
+"""
+Once upon a time, in a [adjective] [noun],
+there lived a [adjective] [noun].
+Every day, the [noun] would [verb] [adverb].
+One day, a [adjective] [noun] appeared
+and [verb] the [noun] away.
+The [noun] was [adjective] and [emotion].
+"""
+
+char1 = input("Who is will this story be about?\n>> ")
